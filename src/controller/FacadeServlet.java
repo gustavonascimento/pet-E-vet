d10391b451bbc24cf7f1c0cf917306b1bae98987
Gustavo Moreira <gustavomn93@gmail.com>
2016-11-27 19:32:38 -0200

    Implemanting Facade pattern general class

---
+++ src/controller/FacadeServlet.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/facadeServlet")
+public class FacadeServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		
+		String task = request.getParameter("task");
+		
+		if(task == null ) {
+			throw new IllegalArgumentException("");
+		}
+		
+		task = "src.controller." + task;
+		
+		try {
+			Class<?> type = Class.forName(task);
+			Task instance = (Task) type.newInstance();
+			String page = instance.execute(request,response);
+			RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+			dispatcher.forward(request, response);
+		} catch(ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			throw new ServletException(e);
+		} 
+		
+	}
+	
+}
